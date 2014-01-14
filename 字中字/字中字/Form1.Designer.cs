6e13c1230b7c9e2e6a98aa4aa4c042231e7c0417
ensleep <ensleep@163.com>
2014-01-14 09:08:05 +0800

    可直接转换已有的bmp图片

--- 字中字/字中字/Form1.Designer.cs
+++ 字中字/字中字/Form1.Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
@@ -43,13 +42,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.textBox1.Location = new System.Drawing.Point(12, 140);
+            this.textBox1.Location = new System.Drawing.Point(3, 73);
             this.textBox1.MaxLength = 100;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(224, 26);
@@ -59,36 +62,28 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Location = new System.Drawing.Point(105, 350);
+            this.pictureBox1.Location = new System.Drawing.Point(191, 367);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(32, 32);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 359);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "转化成图像";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(161, 273);
+            this.button2.Location = new System.Drawing.Point(0, 261);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(139, 23);
             this.button2.TabIndex = 3;
-            this.button2.Text = "图像转为字符";
+            this.button2.Text = "将语句转为图片";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(134)));
             this.textBox2.Location = new System.Drawing.Point(242, 12);
             this.textBox2.Multiline = true;
@@ -102,7 +97,7 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("宋体", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.textBox3.Location = new System.Drawing.Point(12, 187);
+            this.textBox3.Location = new System.Drawing.Point(9, 137);
             this.textBox3.MaxLength = 1;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 80);
@@ -111,7 +106,7 @@
             // textBox4
             // 
             this.textBox4.Font = new System.Drawing.Font("宋体", 64F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.textBox4.Location = new System.Drawing.Point(136, 187);
+            this.textBox4.Location = new System.Drawing.Point(130, 137);
             this.textBox4.MaxLength = 1;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 80);
@@ -120,7 +115,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 169);
+            this.label1.Location = new System.Drawing.Point(7, 113);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 12);
             this.label1.TabIndex = 7;
@@ -129,7 +124,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(134, 169);
+            this.label2.Location = new System.Drawing.Point(128, 113);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 12);
             this.label2.TabIndex = 8;
@@ -138,18 +133,18 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 114);
+            this.label3.Location = new System.Drawing.Point(7, 41);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.Size = new System.Drawing.Size(77, 12);
             this.label3.TabIndex = 9;
-            this.label3.Text = "要转换的字";
+            this.label3.Text = "要转换的语句";
             // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(118, 24);
+            this.comboBox1.Location = new System.Drawing.Point(70, 3);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 20);
             this.comboBox1.TabIndex = 10;
@@ -165,7 +160,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(161, 310);
+            this.button3.Location = new System.Drawing.Point(0, 290);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 12;
@@ -181,31 +176,56 @@
             this.label5.Size = new System.Drawing.Size(0, 12);
             this.label5.TabIndex = 13;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "直接转换已存在的图片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "bmp图片";
+            this.openFileDialog1.Title = "请选择要转换的bmp图片";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(6, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 390);
+            this.panel1.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(806, 501);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "字中字生成器 by ensleep";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,7 +235,6 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
@@ -227,6 +246,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- 字中字/字中字/Form1.cs
+++ 字中字/字中字/Form1.cs
@@ -15,7 +15,7 @@ namespace 字中字
     public partial class Form1 : Form
     {
         System.Drawing.Bitmap bmp;
-        int size = 32;
+        int imgh = 32;
         public Form1()
         {
             InitializeComponent();
@@ -35,14 +35,14 @@ namespace 字中字
             }
             else
             {
-                Font f = new System.Drawing.Font(comboBox1.SelectedItem.ToString().Trim(), size / 2, FontStyle.Regular);
-                bmp = new System.Drawing.Bitmap(size * textBox1.Text.Trim().Length, size);
+                Font f = new System.Drawing.Font(comboBox1.SelectedItem.ToString().Trim(), imgh / 2, FontStyle.Regular);
+                bmp = new System.Drawing.Bitmap(imgh * textBox1.Text.Trim().Length, imgh);
                 //Image image = new System.Drawing.Bitmap(32,32);
                 Graphics gra = Graphics.FromImage(bmp);
-                System.Drawing.Drawing2D.LinearGradientBrush b = new System.Drawing.Drawing2D.LinearGradientBrush(new Rectangle(0, 0, size * textBox1.Text.Trim().Length, size), Color.Black, Color.Black, 0, false);
+                System.Drawing.Drawing2D.LinearGradientBrush b = new System.Drawing.Drawing2D.LinearGradientBrush(new Rectangle(0, 0, imgh * textBox1.Text.Trim().Length, imgh), Color.Black, Color.Black, 0, false);
                 gra.DrawString(textBox1.Text.Trim(), f, b, 0, 0);
-                pictureBox1.Height = size;
-                pictureBox1.Width = size * textBox1.Text.Trim().Length;
+                pictureBox1.Height = imgh;
+                pictureBox1.Width = imgh * textBox1.Text.Trim().Length;
                 pictureBox1.Image = bmp;
                 if (File.Exists("tmp.bmp"))
                 {
@@ -55,51 +55,59 @@ namespace 字中字
         {
 
         }
-        public void GetChar()
+        public void GetChar(string path)
         {
-
-            Stream stream = File.OpenRead("tmp.bmp");  // 打开位图文件
-            byte[] buffer = new byte[stream.Length - 54];  // 缓冲区，文件长度减去文件头和信息头的长度
-
-            stream.Position = 54;  // 跳过文件头和信息头
-
-            stream.Read(buffer, 0, buffer.Length);  // 读取位图数据，位图数据是颠倒的
-            byte[] buf = new byte[buffer.Length / 4];
-            for (int a = 3, i = 0; a < buffer.Length; a+=4,i++)
+            try
             {
-                buf[i] = buffer[a];
+                Stream stream = File.OpenRead(path);  // 打开位图文件
+                byte[] buffer = new byte[stream.Length - 54];  // 缓冲区，文件长度减去文件头和信息头的长度
+
+                stream.Position = 54;  // 跳过文件头和信息头
+
+                stream.Read(buffer, 0, buffer.Length);  // 读取位图数据，位图数据是颠倒的
+                byte[] buf = new byte[buffer.Length / 4];
+                Image img = Bitmap.FromStream(stream);
+                imgh = img.Height;
+                for (int a = 3, i = 0; a < buffer.Length; a += 4, i++)
+                {
+                    buf[i] = buffer[a];
+                }
+                StringBuilder[] sbArray = new StringBuilder[imgh];
+                StringBuilder sb = new StringBuilder();
+                int count = -1;
+                for (int a = 0; a < buf.Length; a++)
+                {
+                    if (a % (buf.Length / imgh) == 0)
+                    {
+                        count++;
+                        sbArray[count] = new StringBuilder();
+                    }
+                    if (buf[a] == 255)
+                    {
+                        sbArray[count].Append(GetSafeString(textBox3.Text.ToCharArray().Length > 0 ? textBox3.Text.ToCharArray()[0].ToString() : "x"));
+                    }
+                    else
+                    {
+                        sbArray[count].Append(GetSafeString(textBox4.Text.ToCharArray().Length > 0 ? textBox4.Text.ToCharArray()[0].ToString() : " "));
+                    }
+                }
+                for (int a = sbArray.Length - 1; a >= 0; a--)
+                {
+                    textBox2.Text += sbArray[a].ToString();
+                    textBox2.Text += "\r\n";
+                }
+                stream.Close();
+                stream.Dispose();
             }
-            StringBuilder[] sbArray = new StringBuilder[ size];
-            StringBuilder sb = new StringBuilder();
-            int count = -1;
-            for (int a = 0; a < buf.Length; a++)
+            catch (Exception ex)
             {
-                if (a % (buf.Length / size) == 0)
-                {
-                    count++;
-                    sbArray[count] = new StringBuilder();
-                }
-                if (buf[a] == 255)
-                {
-                    sbArray[count].Append(GetSafeString(textBox3.Text.ToCharArray().Length > 0 ? textBox3.Text.ToCharArray()[0].ToString() : "x"));
-                }
-                else
-                {
-                    sbArray[count].Append(GetSafeString(textBox4.Text.ToCharArray().Length > 0 ? textBox4.Text.ToCharArray()[0].ToString() : " "));
-                }
+                MessageBox.Show(ex.ToString());
             }
-            for (int a = sbArray.Length - 1; a >= 0; a--)
-            {
-                textBox2.Text += sbArray[a].ToString();
-                textBox2.Text += "\r\n";
-            }
-            stream.Close();
-            stream.Dispose();
         }
         private void button2_Click(object sender, EventArgs e)
         {
             CreateBmp();
-            GetChar();
+            GetChar("tmp.bmp");
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -126,6 +134,24 @@ namespace 字中字
             if (c < 127)
                 c = (char)(c + 65248);
             return c.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".bmp";
+            openFileDialog1.CheckPathExists = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                if (path.Split('.').LastOrDefault() == "bmp")
+                {
+                    GetChar(path);
+                }
+                else
+                {
+                    MessageBox.Show("打开的图片格式不正确");
+                }
+            }
         }
     }
 }
